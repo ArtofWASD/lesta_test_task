28b012e25465ec0b3c59c76b1beb806c2cca2c74
Leonid <Chunl3e666@yandex.ru>
2023-10-25 17:50:14 +0300

    муки совести с внешним видом

--- components/allShipsTable/allShipsTable.tsx
+++ components/allShipsTable/allShipsTable.tsx
@@ -6,15 +6,15 @@ const AllShipsTable = ({ data }: any) => {
   return (
     <div className="grid grid-flow-row max-w-screen-md justify-center items-center py-4">
       <div className="flex gap-6 justify-between">
-        <div className="flex flex-col items-center">
+        <div className="flex gap-2 justify-center items-center">
           <p>Уровень корабля </p>
           <PropertyListBox />
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex gap-2 justify-center items-center">
           <p>Нация </p>
           <PropertyListBox />
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex gap-2 justify-center items-center">
           <p>Класс </p>
           <PropertyListBox />
         </div>
